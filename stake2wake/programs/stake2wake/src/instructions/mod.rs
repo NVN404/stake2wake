81f4c10ffe2d6f38b06114502210058461021e5a
NVN404 <itsmenaveen404@gmail.com>
2025-07-14 11:32:12 +0530

    feat:  update mod.rs

--- stake2wake/programs/stake2wake/src/instructions/mod.rs
+++ stake2wake/programs/stake2wake/src/instructions/mod.rs
@@ -2,11 +2,9 @@
 pub mod initialize;
 pub mod start_challenge;
 pub mod check_status;
-pub mod claim_reward;
 pub mod cancel_challenge;
 
 pub use initialize::*;
 pub use start_challenge::*;
 pub use check_status::*;
-pub use claim_reward::*;
 pub use cancel_challenge::*;
