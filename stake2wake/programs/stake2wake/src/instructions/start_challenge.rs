818005ac6702db40ecd0dc463a11371d5988ef84
JKrishnaD <jayak5063@gmail.com>
2025-07-19 18:57:30 +0530

    fix:bugs in contract

--- stake2wake/programs/stake2wake/src/instructions/start_challenge.rs
+++ stake2wake/programs/stake2wake/src/instructions/start_challenge.rs
@@ -1,14 +1,15 @@
 use anchor_lang::prelude::*;
 use anchor_spl::{
     associated_token::AssociatedToken,
-    token::{ transfer_checked, Token, TokenAccount, TransferChecked },
+    token::{transfer_checked, Token, TokenAccount, TransferChecked},
     token_interface::Mint,
 };
 
-use crate::state::ChallengeAccount;
 use crate::error::Stake2WakeError;
+use crate::state::ChallengeAccount;
 
 #[derive(Accounts)]
+#[instruction(start_time: u64)]
 pub struct StartChallenge<'info> {
     // the person who is starting the challenge
     #[account(mut)]
@@ -19,7 +20,7 @@ pub struct StartChallenge<'info> {
         init,
         payer = user,
         space = 8 + ChallengeAccount::INIT_SPACE, // 8 bytes for discriminator + struct size
-        seeds = [b"challenge", user.key().as_ref(), clock.unix_timestamp.to_le_bytes().as_ref()], // using clock here to allow multiple challenges
+        seeds = [b"challenge", user.key().as_ref(), start_time.to_le_bytes().as_ref()], // using clock here to allow multiple challenges
         bump
     )]
     pub user_challenge: Account<'info, ChallengeAccount>, // we are creating account for userchallenge first
@@ -56,10 +57,11 @@ pub struct StartChallenge<'info> {
 impl<'info> StartChallenge<'info> {
     pub fn start_challenge(
         &mut self,
-        wakeup_time: u64, // When the user wants to wake up
+        start_time: u64,
+        wakeup_time: u64,  // When the user wants to wake up
         stake_amount: u64, // Amount user is staking
-        total_days: u64, // Number of days challenge lasts
-        bumps: &StartChallengeBumps
+        total_days: u64,   // Number of days challenge lasts
+        bumps: &StartChallengeBumps,
     ) -> Result<()> {
         require!(stake_amount > 0, Stake2WakeError::InvalidStakeAmount); // checking for the valid stake amount
         require!(total_days > 0, Stake2WakeError::InvalidTotalDays); // checking for the valid total days
@@ -67,8 +69,6 @@ impl<'info> StartChallenge<'info> {
             wakeup_time > (self.clock.unix_timestamp as u64),
             Stake2WakeError::InvalidWakeupTime
         ); // checking for the valid wakeup time
-
-        let now = self.clock.unix_timestamp as u64;
 
         // creating an instance using set_inner method to update all the fileds at once
         self.user_challenge.set_inner(ChallengeAccount {
@@ -78,9 +78,9 @@ impl<'info> StartChallenge<'info> {
             mint: self.bonk_mint.key(),
             vault: self.vault.key(),
             is_active: true,
-            start_time: now,
-            end_time: now + total_days * 86400, // 86400 seconds in a day
-            last_check_time: 0, // Initialize last check time to 0
+            start_time,
+            end_time: start_time + total_days * 86400, // 86400 seconds in a day
+            last_check_time: 0,                        // Initialize last check time to 0
             completed_days: 0,
             total_days,
             bump: bumps.user_challenge,
